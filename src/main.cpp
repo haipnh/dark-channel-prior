4164cf2f767b0d0a3f63b541681dbca9b4b25cf8
Hai Pham <haiphamnguyenhoang@gmail.com>
2022-04-07 11:02:59 +0800

    restructure program

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <iostream>
 #include <string>
+#include <chrono>
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,6 +12,7 @@
 
 using namespace cv;
 using namespace std;
+using namespace std::chrono;
 
 #define DRK_CH_WND_WT 5 //should be an odd number
 #define DRK_CH_WND_HT 5 //should be an odd number
@@ -34,20 +36,52 @@ int main( int argc, char** argv )
     int img_width = image.size().width;
     int img_height = image.size().height;
 
-    cv::split(image, channels); // split from BGR image to RGB channels
-
+    // INIT: dark channel calculation
     unsigned char* dark_channel = (unsigned char *)malloc(sizeof(unsigned char)*img_height*img_width);
 
     int h=0, w=0;
     int i=0, j=0;
-    int min_ri=255, min_gi=255, min_bi=255;
+    unsigned char pix_r=0, pix_g=0, pix_b=0;
+    unsigned char min_ri=255, min_gi=255, min_bi=255;
     int lh=0, lw=0;
-
-    // calculate dark channel
+    
     struct pix_t center;
     center.h = DRK_CH_WND_HT/2;
     center.w = DRK_CH_WND_WT/2;
 
+    // INIT: pick up the top p% brightest pixels in the dark channel
+    float p=0.1;
+    int max_val=255;
+    int num_of_pix=(int)((img_height)*(img_width)*p)/100;
+    struct pix_t *array_pix = (struct pix_t *)malloc((num_of_pix)*sizeof(struct pix_t));
+
+    // INIT: find the airlight which has the hightest intensity in the input image
+    unsigned char* yuv_y = (unsigned char *)malloc(sizeof(unsigned char)*img_height*img_width);
+    unsigned char* yuv_u = (unsigned char *)malloc(sizeof(unsigned char)*img_height*img_width);
+    unsigned char* yuv_v = (unsigned char *)malloc(sizeof(unsigned char)*img_height*img_width);
+    int max_y=0;
+    struct pix_t pos;
+    unsigned char airlight_r;
+    unsigned char airlight_g;
+    unsigned char airlight_b;
+
+    // INIT: compute the transmission map
+    float omega=0.95;
+    float min_r,min_g,min_b,min_t;
+    float *tmap = (float*)malloc(sizeof(float)*img_height*img_width);
+
+    // INIT: compute the haze free image
+    float t0=0.01;
+    float r,g,b,t_;
+    unsigned char* output = (unsigned char*)malloc(sizeof(unsigned char)*img_height*img_width*3);
+    Mat dehaze_mat(img_height, img_width, CV_8UC3, output);
+
+    // Begin: Pre-Processing
+    auto start = high_resolution_clock::now();
+
+    cv::split(image, channels); // split from BGR image to RGB channels
+
+    // calculate dark channel
     for(int h = 0; h < image.rows; h++)
     {
         for(int w = 0; w < image.cols; w++)
@@ -57,21 +91,21 @@ int main( int argc, char** argv )
             for(lh=-center.h;lh<=center.h;lh++){ // -2 -> +2
                 for(lw=-center.w;lw<=center.w;lw++){ // -2 -> +2
                     if((w+lw)>=0&&(w+lw)<img_width-1&&(h+lh)>=0&&(h+lh)<img_height-1){ // ignore out-of-frame pixels
-                        min_ri=MIN(min_ri, channels[0].data[img_width*(h+lh)+(w+lw)]);
-                        min_gi=MIN(min_gi, channels[1].data[img_width*(h+lh)+(w+lw)]);
-                        min_bi=MIN(min_bi, channels[2].data[img_width*(h+lh)+(w+lw)]);
+                        pix_r = channels[0].data[img_width*(h+lh)+(w+lw)];
+                        pix_g = channels[1].data[img_width*(h+lh)+(w+lw)];
+                        pix_b = channels[2].data[img_width*(h+lh)+(w+lw)];
+
+                        min_ri=MIN(min_ri, pix_r);
+                        min_gi=MIN(min_gi, pix_g);
+                        min_bi=MIN(min_bi, pix_b);
                     }
                 }
             }
             dark_channel[img_width*h+w] = MIN(MIN(min_ri,min_gi),min_bi);
         }
     }
-
+    
     // pick up the top p% brightest pixels in the dark channel
-    float p=0.1;
-    int max_val=255;
-    int num_of_pix=(int)((img_height)*(img_width)*p)/100;
-    struct pix_t *array_pix = (struct pix_t *)malloc((num_of_pix)*sizeof(struct pix_t));
     for(i=0;i<num_of_pix;){
         for(h=0;h<img_height;h++){
             for(w=0;w<img_width;w++){
@@ -90,11 +124,11 @@ int main( int argc, char** argv )
             break;
         max_val--;
     }
-
+    
     // find the airlight which has the hightest intensity in the input image
-    unsigned char* yuv_y = (unsigned char *)malloc(sizeof(unsigned char)*img_height*img_width);
-    unsigned char* yuv_u = (unsigned char *)malloc(sizeof(unsigned char)*img_height*img_width);
-    unsigned char* yuv_v = (unsigned char *)malloc(sizeof(unsigned char)*img_height*img_width);
+    airlight_r = channels[0].data[img_width*pos.h+pos.w];
+    airlight_g = channels[1].data[img_width*pos.h+pos.w];
+    airlight_b = channels[2].data[img_width*pos.h+pos.w];
       // convert rgb to yuv
       for(h=0;h<img_height;h++){
           for(w=0;w<img_width;w++){
@@ -103,22 +137,14 @@ int main( int argc, char** argv )
               yuv_v[img_width*h+w]=((112*channels[0].data[img_width*h+w] -94*channels[1].data[img_width*h+w] -18*channels[2].data[img_width*h+w]+128)>>8)+128;
           }
       }
-    int max_y=0;
-    struct pix_t pos;
     for(i=0;i<num_of_pix;i++){
         if ( max_y < yuv_y[img_width * array_pix[i].h + array_pix[i].w]){
             pos=array_pix[i];
             max_y=yuv_y[img_width*pos.h+pos.w];
         }
     }
-    unsigned char airlight_r = channels[0].data[img_width*pos.h+pos.w];
-    unsigned char airlight_g = channels[1].data[img_width*pos.h+pos.w];
-    unsigned char airlight_b = channels[2].data[img_width*pos.h+pos.w];
-
+    cout << "Checkpoint\n";
     // compute the transmission map
-    float omega=0.95;
-    float min_r,min_g,min_b,min_t;
-    float *tmap = (float*)malloc(sizeof(float)*img_height*img_width);
     for(h=0;h<img_height;h++){
         for(w=0;w<img_width;w++){
             min_r=255.0;
@@ -133,19 +159,12 @@ int main( int argc, char** argv )
                     }
                 }
             }
-
             min_t=MIN(MIN(min_r,min_g),min_b);
             tmap[img_width*h+w]=1-omega*min_t;
         }
     }
 
     // compute the haze free image
-    float t0=0.01;
-    float r,g,b,t_;
-
-    unsigned char* output = (unsigned char*)malloc(sizeof(unsigned char)*img_height*img_width*3);
-    Mat dehaze_mat(img_height, img_width, CV_8UC3, output);
-
     for(h=0;h<img_height;h++){
         unsigned char *rowPtr = dehaze_mat.ptr<unsigned char>(h);
         for(w=0;w<img_width;w++){
@@ -160,6 +179,12 @@ int main( int argc, char** argv )
             rowPtr[w*3+2] = (unsigned char)MIN(MAX(b,0),255); // B
         }
     }
+
+    // End: Pre-Processing
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start);
+    cout << "Processing time: " << duration.count() << " us." << endl;
+    cout << "Expected       : 16667 us." << endl;
 
 #if DISPLAY
     namedWindow("1-Input", WINDOW_AUTOSIZE);
@@ -178,8 +203,5 @@ int main( int argc, char** argv )
 
     waitKey(0);
 #endif
-
-    cout << "Done!\n";
-
     return 0;
 }
